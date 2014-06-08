2141afb515136d60819df11fb6548035ca653494
denisov <elgato.andrey@gmail.com>
2014-06-08 20:27:18 +0700

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,4 @@
+alert('a');
 <?php
 
 /*
@@ -9,12 +10,12 @@
  * @author Andrey
  */
 // TODO: check include path
-ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../${php.global.include.path}');
+//ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../${php.global.include.path}');
 
 // put your code here
-session_start();
+//session_start();
 
-if(!empty($_SESSION)) die('$_SESSION should be empty for proper testing. Something goes wrong');
+//if(!empty($_SESSION)) die('$_SESSION should be empty for proper testing. Something goes wrong');
 
 
 ?>
